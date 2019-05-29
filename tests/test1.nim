2b352575fa7bb4acc0c5a22498003dbc9a8fb857
Kaushal Modi <kaushal.modi@gmail.com>
2019-05-28 21:46:00 -0400

    Update test; add case where a ztag field value has spaces

--- tests/test1.nim
+++ tests/test1.nim
@@ -6,46 +6,13 @@ suite "ztag to json":
     const
       cwd = currentSourcePath.parentDir()
       sampleZtagFile = cwd / "sample.ztag"
-      sampleJsonFile = cwd / "sample.json"
-      refJson = """[
-  {
-    "depotFile": "//abc/def/ghi/TRUNK/jkl/mno/pqr.stu",
-    "clientFile": "//ws:vwx:def.yz:7434600b/ghi/jkl/mno/pqr.stu",
-    "rev": "7",
-    "haveRev": "3",
-    "action": "edit",
-    "change": "568524",
-    "type": "text",
-    "user": "vwx",
-    "client": "ws:vwx:def.yz:0680383b",
-    "payload": "payload of first record1\n\npayload of first record2\npayload of first record3"
-  },
-  {
-    "depotFile": "//abc/def/ghi/TRUNK/jkl/mno/PQR.STU",
-    "clientFile": "//ws:vwx:def.yz:6824560b/ghi/jkl/mno/PQR.STU",
-    "rev": "1",
-    "haveRev": "6",
-    "action": "edit",
-    "change": "085030",
-    "type": "text",
-    "user": "vwx",
-    "client": "ws:vwx:def.yz:8464768b",
-    "payload": "payload of second record1\n\npayload of second record2\npayload of second record3\n"
-  },
-  {
-    "abc": "d/e/f",
-    "ghi": "1.2.3"
-  },
-  {
-    "abc": "j:k:l",
-    "mno": "456.789"
-  }
-]"""
+      refJson = readFile(cwd / "reference.json")
+      outputJsonFile = cwd / "sample.json"
 
   test "ztag string to JSON":
     check readFile(sampleZtagFile).ztagStringToJson() == refJson
 
   test "ztag file to JSON":
     ztagFileToJson(sampleZtagFile)
-    check readFile(sampleJsonFile) == refJson
-    removeFile(sampleJsonFile)
+    check readFile(outputJsonFile) == refJson
+    removeFile(outputJsonFile)
